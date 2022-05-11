a5b0294ec706d4a09bb3ce30b2560fbaf6e1a225
savan <kansagrasavan108@gmail.com>
2022-05-11 09:41:49 +0530

    finding the minumum distance

---
+++ src/week4_divide_and_conquer/MinimumDistInPoints.java
@@ -0,0 +1,10 @@
+package week4_divide_and_conquer;
+
+public class MinimumDistInPoints {
+	public static void main(String[] args) {
+		MinimumDistInPoints minimumDistInPoints = new MinimumDistInPoints();
+		
+		/* */
+		
+	}
+}
